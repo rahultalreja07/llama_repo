9c3cd707c6935b72f4ba6b255ed9f720c195a53e
rtalrej <Rahul.Talreja@morningstar.com>
2024-07-22 19:57:53 +0530

    3rd commit

--- prompts/CodeCompass/LogAnalysisPrompt.js
+++
@@ -1,8 +0,0 @@
-const techArchitecturePrompt = (userInput) => `
-Please analyse and summarize the following logs and then provide your suggestions on promblamtic areas with troubleshooting tips:
-
----
-${userInput}
----
-`
-export default techArchitecturePrompt;
